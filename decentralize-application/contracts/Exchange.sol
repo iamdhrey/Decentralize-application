ef339950639df5a63ab10089fa15241870c94744
iamdhrey <damilare.nelson@outlook.com>
2023-09-20 11:02:36 -0600

    Create exchane and Fee Percent

---
+++ decentralize-application/contracts/Exchange.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Exchange {
+    address public feeAccount;
+    uint256 public feePercent;
+
+    constructor(address _feeAccount, uint256 _feePercent) {
+        feeAccount = _feeAccount;
+        feePercent = _feePercent;
+    }
+}
+
+
+
+//CHECKLIST
+    //Deposit
+    //Withdraw
+    //Check Balance
+    //Make order
+    //Cancel order
+    //Fill Orders
+    //Charge Fees
+    //Track Fee Account
